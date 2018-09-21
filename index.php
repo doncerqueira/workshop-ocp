35619e404be4d50ec9e86ec0f2bf65743a022251
doncerqueira <43449223+doncerqueira@users.noreply.github.com>
2018-09-21 11:47:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 //echo $_SERVER['SERVER_ADDR'];
 //?>*/
 <?php
-echo "<h1>Openshift Workshop v2.0</h1> ";
+echo "<h1>Openshift Workshop v3.0</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 echo "<br><hr>";
 echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
